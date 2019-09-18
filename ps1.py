0cbca6c6cd57f3c5083c535d4b3e0816afd5ad94
Ankit Sharma <ankit3e@gmail.com>
2019-09-18 10:02:32 +0530

    ps1 comments

--- ps1.py
+++ ps1.py
@@ -6,13 +6,13 @@ oneCount = 0;
 #--------Parity Bit calculation---------
 for i in range(length):
 	if bitStr[i] == '1':
-		oneCount += 1
+		oneCount += 1		#Increment '1' counter
 	elif bitStr[i] != '0':
-		print("Wrong input")
-		exit()
+		print("Wrong input")	#Display error for non-binary input
+		exit()			#Quit on error
 # Appending corresponding parity bit
 if oneCount % 2 == 0:
-	bitStr = bitStr + '1'
+	bitStr = bitStr + '1'		#Appending parity bit
 else:
 	bitStr = bitStr + '0'
 	
@@ -21,16 +21,14 @@ print("Parity Bt Data :",bitStr)
 #------------ Bit Stuffing-------------
 length = len(bitStr)
 txData = ''
-temp = ''
-pat =str('010')
-done =0
+pat =str('010')				# Pattern to be identified
 while 1:
-	ind =  bitStr.find(pat)	 
-	if ind==-1:
-		break
-	txData += bitStr[0:ind+3]
-	txData += '0'
-	bitStr = bitStr[ind+3:len(bitStr)]
-txData += bitStr
-txData += '0101'
-print("Transmitting Data :",txData)
+	ind =  bitStr.find(pat)	 	# Find first occurrence of pattern
+	if ind==-1:			# Break if no pattern found
+		break		
+	txData += bitStr[0:ind+3]	# Append the data upto pattern
+	txData += '0'			# Stuff '0' bit
+	bitStr = bitStr[ind+3:len(bitStr)]	# Update the original data
+txData += bitStr			# Append the remaining data
+txData += '0101'			# Ending flag
+print("Transmitting Data :",txData)	
